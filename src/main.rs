6bb19751c7792afcdcc241891033cc93f46ef09e
Jean-Marc Alliot <jm@alliot.org>
2022-12-14 23:19:41 +0100

    New version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 /*
+exiftool -geotag=Louisiane.gpx -geosync=+6:00:00 .
 sudo mount -t tmpfs -o size=1g tmpfs /mnt/ramfs
 convert -background black -fill white -size 480x1080  -gravity center  label:"Ville Plate\nUnited States"   label_size.jpg
 darktable-cli --height 1080 L1004100.DNG toto.jpg
@@ -44,16 +45,18 @@ fn read_cities(file_path: &str) -> Vec<City> {
     tab
 }
 
-fn get_latlon(path: &str) -> (f64,f64) {
+fn get_latlon(path: &str) -> (f64,f64,String) {
     let file = std::fs::File::open(path).unwrap();
     let mut bufreader = std::io::BufReader::new(&file);
     let exifreader = exif::Reader::new();
     let exif = exifreader.read_from_container(&mut bufreader).unwrap();
     let mut lat  =0.;
     let mut lon =0.;
+    let mut s= "".to_string();
     for f in exif.fields() {
 	match f.tag.description() {
 	    Some(t) => {
+//		println!("{:?} {}",t,f.display_value().with_unit(&exif).to_string());
 		if t.eq("Latitude") {
 		    let s = f.display_value().with_unit(&exif).to_string();
 		    let v: Vec<&str> = s.split(' ').collect();
@@ -72,11 +75,15 @@ fn get_latlon(path: &str) -> (f64,f64) {
 			lon = -lon;
 		    }
 		}
+		if t.eq("Date and time of original data generation")
+		{
+		    s = f.display_value().with_unit(&exif).to_string();
+		}
 	    },
 	    None => {}
 	}
     }
-    return (lat,lon);
+    return (lat,lon,s);
 }
 
 fn deg2rad(deg:f64) -> f64 {
@@ -98,7 +105,20 @@ fn dist(lat1:f64,lon1:f64,lat2:f64,lon2:f64) -> f64{
 
 fn main() {
     let tab = read_cities("cities.csv");
-    let (lat,lon)=get_latlon(&"L1004163.DNG");
+
+    //    darktable-cli --height 1080 L1004100.DNG toto.jpg
+    let path = "L1004163.DNG";
+    let status = std::process::Command::new("/usr/bin/darktable-cli")
+        .args([
+	    "--height", "1080",
+	    &path,
+	    "/mnt/ramfs/toto.jpg"
+	])
+        .status()
+        .expect("failed to execute process");
+    println!("process finished with: {status}");
+    
+    let (lat,lon,date)=get_latlon(&"/mnt/ramfs/toto.jpg");
     let r = tab.iter().min_by_key(
 	|x| {
 	    let d= dist(lat,lon,x.lat,x.lon);
@@ -106,5 +126,42 @@ fn main() {
 	    d as i64
 	}
     ).unwrap();
-    println!("{} {} {:?} {}",lat,lon,r,dist(lat,lon,r.lat,r.lon));
+//    println!("{} {} {}",date,r.city,r.country);
+    let v: Vec<&str> = date.split(' ').collect();
+    let s = "label:".to_owned()+v[0]+"\n"+v[1]+"\n"+&r.city+"\n"+&r.country;
+    println!("{}",s);
+//    convert -background black -fill white -size 480x1080  -gravity center  label:"Ville Plate\nUnited States"   label_size.jpg
+    let status = std::process::Command::new("/usr/bin/convert")
+        .args([
+	    "-background", "black",
+	    "-fill", "white",
+	    "-size", "480x1080",
+	    "-gravity", "center",
+	    &s  ,
+	    "/mnt/ramfs/label_size.jpg"
+	])
+        .status()
+        .expect("failed to execute process");
+    println!("process finished with: {status}");
+
+//    convert +append label_size.jpg toto.jpg out.jpg
+    let status = std::process::Command::new("/usr/bin/convert")
+        .args([
+	    "+append",
+	    "/mnt/ramfs/toto.jpg",
+	    "/mnt/ramfs/label_size.jpg",
+	    "/mnt/f/jpegs/out.jpg"
+	])
+        .status()
+        .expect("failed to execute process");
+    println!("process finished with: {status}");
+
+    let status = std::process::Command::new("/usr/bin/rm")
+        .args([
+	    "/mnt/ramfs/toto.jpg",
+	    "/mnt/ramfs/label_size.jpg"
+	])
+        .status()
+        .expect("failed to execute process");
+    println!("process finished with: {status}");
 }
