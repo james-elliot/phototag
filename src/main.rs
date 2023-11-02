0e6c8917b0219b2a3a3007fc57da852576fe3056
Jean-Marc Alliot <jm@alliot.org>
2023-11-02 17:04:37 +0100

    Rename automatically files to the shooting date. Added options and various safeguards.

--- src/main.rs
+++ src/main.rs
@@ -44,42 +44,48 @@ fn get_latlon(path: &str) -> Option<(f64, f64, String)> {
     let file = std::fs::File::open(path).unwrap();
     let mut bufreader = std::io::BufReader::new(&file);
     let exifreader = exif::Reader::new();
-    let exif = exifreader.read_from_container(&mut bufreader).unwrap();
-    let mut lat = 0.;
-    let mut lon = 0.;
-    let mut s = "".to_string();
-    for f in exif.fields() {
-        if let Some(t) = f.tag.description() {
-            //		println!("{:?} {}",t,f.display_value().with_unit(&exif).to_string());
-            if t.eq("Latitude") {
-                let s = f.display_value().with_unit(&exif).to_string();
-                let v: Vec<&str> = s.split(' ').collect();
-                lat = v[0].parse::<f64>().unwrap()
-                    + v[2].parse::<f64>().unwrap() / 60.
-                    + v[4].parse::<f64>().unwrap() / 3600.;
-                if v[6].eq("S") {
-                    lat = -lat;
-                }
-            }
-            if t.eq("Longitude") {
-                let s = f.display_value().with_unit(&exif).to_string();
-                let v: Vec<&str> = s.split(' ').collect();
-                lon = v[0].parse::<f64>().unwrap()
-                    + v[2].parse::<f64>().unwrap() / 60.
-                    + v[4].parse::<f64>().unwrap() / 3600.;
-                if v[6].eq("W") {
-                    lon = -lon;
-                }
-            }
-            if t.eq("Date and time of original data generation") {
-                s = f.display_value().with_unit(&exif).to_string();
-            }
-        }
+//    let exif = exifreader.read_from_container(&mut bufreader).unwrap();
+    let exif_res = exifreader.read_from_container(&mut bufreader);
+    match exif_res {
+	Err(_) => return None,
+	Ok(exif) => {
+	    let mut lat = 0.;
+	    let mut lon = 0.;
+	    let mut s = "".to_string();
+	    for f in exif.fields() {
+		if let Some(t) = f.tag.description() {
+		    //		eprintln!("{:?} {}",t,f.display_value().with_unit(&exif).to_string());
+		    if t.eq("Latitude") {
+			let s = f.display_value().with_unit(&exif).to_string();
+			let v: Vec<&str> = s.split(' ').collect();
+			lat = v[0].parse::<f64>().unwrap()
+			    + v[2].parse::<f64>().unwrap() / 60.
+			    + v[4].parse::<f64>().unwrap() / 3600.;
+			if v[6].eq("S") {
+			    lat = -lat;
+			}
+		    }
+		    if t.eq("Longitude") {
+			let s = f.display_value().with_unit(&exif).to_string();
+			let v: Vec<&str> = s.split(' ').collect();
+			lon = v[0].parse::<f64>().unwrap()
+			    + v[2].parse::<f64>().unwrap() / 60.
+			    + v[4].parse::<f64>().unwrap() / 3600.;
+			if v[6].eq("W") {
+			    lon = -lon;
+			}
+		    }
+		    if t.eq("Date and time of original data generation") {
+			s = f.display_value().with_unit(&exif).to_string();
+		    }
+		}
+	    }
+	    if lat == 0. {
+		return None;
+	    };
+	    Some((lat, lon, s))
+	}
     }
-    if lat == 0. {
-        return None;
-    };
-    Some((lat, lon, s))
 }
 
 fn deg2rad(deg: f64) -> f64 {
@@ -96,32 +102,40 @@ fn dist(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> f64 {
     r * c // Distance in km
 }
 
-fn one(p: &std::path::Path, tab: &[City], ext: &str) {
-    let p1 = p.file_stem().and_then(std::ffi::OsStr::to_str);
+fn one(p: &std::path::Path, tab: &[City], ext: &str, output_dir: &str) {
+    let _p1 = p.file_stem().and_then(std::ffi::OsStr::to_str);
     let p2 = p.extension().and_then(std::ffi::OsStr::to_str);
     match p2 {
-        None => return,
+        None => {
+	    eprintln!("Not the right name{:?}",p);
+	    return
+	},
         Some(s) => {
             let s = s.to_ascii_lowercase();
             if !s.eq(ext) {
+		eprintln!("Not the right name{:?}",p);
                 return;
             }
-        }
+	}
     }
     let path = p.to_str().unwrap();
     if let Some((lat, lon, date)) = get_latlon(path) {
         let status = std::process::Command::new("/usr/bin/darktable-cli")
-            .args(["--height", "1080", path, "/mnt/ramfs/toto.jpg"])
+            .args(["--width","1620","--height", "1080", path, "/mnt/ramfs/toto.jpg"])
             .status()
-            .expect("failed to execute process");
-        println!("process darktable finished with: {status}");
+            .expect("failed to execute process darktable-cli");
+        if !status.success() {
+	    eprintln!("process darktable finished with status {} for file {:?}",status,p);
+	    return;
+	}
         let r = tab
             .iter()
             .min_by_key(|x| dist(lat, lon, x.lat, x.lon) as i64)
             .unwrap();
         let v: Vec<&str> = date.split(' ').collect();
+	let v0: Vec<&str> = v[0].split('-').collect();
+	let v1: Vec<&str> = v[1].split(':').collect();
         let s = "label:".to_owned() + v[0] + "\n" + v[1] + "\n" + &r.city + "\n" + &r.country;
-        println!("{}", s);
         let status = std::process::Command::new("/usr/bin/convert")
             .args([
                 "-background",
@@ -129,16 +143,19 @@ fn one(p: &std::path::Path, tab: &[City], ext: &str) {
                 "-fill",
                 "white",
                 "-size",
-                "480x1080",
+                "300x1080",
                 "-gravity",
                 "center",
                 &s,
                 "/mnt/ramfs/label_size.jpg",
             ])
             .status()
-            .expect("failed to execute process");
-        println!("process label finished with: {status}");
-        let s = "/mnt/f/jpegs/".to_owned() + p1.unwrap() + ".jpg";
+            .expect("failed to execute process convert");
+        if !status.success() {
+	    eprintln!("process convert finished with status {} for file {:?}",status,p);
+	    return;
+	}
+	let s = output_dir.to_owned() + v0[0]+v0[1]+v0[2]+v1[0]+v1[1]+v1[2] + ".jpg";
         let status = std::process::Command::new("/usr/bin/convert")
             .args([
                 "+append",
@@ -147,28 +164,53 @@ fn one(p: &std::path::Path, tab: &[City], ext: &str) {
                 &s,
             ])
             .status()
-            .expect("failed to execute process");
-        println!("process append finished with: {status}");
-        let status = std::process::Command::new("/usr/bin/rm")
-            .args(["/mnt/ramfs/toto.jpg"])
-            .status()
-            .expect("failed to execute process");
-        println!("process rm finished with: {status}");
+            .expect("failed to execute process append");
+        if !status.success() {
+	    eprintln!("process append finished with status {} for file {:?}",status,p);
+	    return;
+	}
+	std::fs::remove_file("/mnt/ramfs/toto.jpg").expect("Can't remove file");
+	std::fs::remove_file("/mnt/ramfs/label_size.jpg").expect("Can't remove file");
+    }
+    else {
+	eprintln!("Can't get lat/lon for {:?}",p);
     }
 }
 
+use argparse::{ArgumentParser, Store};
 fn main() {
-    let tab = read_cities("cities.csv");
-    for entry in walkdir::WalkDir::new("/mnt/home2/Photos/JMA/M9")
+    let mut output_dir = "/mnt/f/jpegs/".to_string();
+    let mut cities = "cities.csv".to_string();
+    let mut search_dir = ".".to_string();
+    let mut nb_levels = 1;
+
+    { // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Build photos for slideshows");
+        ap.refer(&mut nb_levels)
+            .add_option(&["-n","--nb_levels"], Store,"Number of levels to recurse during directory search (default 1)");
+        ap.refer(&mut search_dir)
+            .add_option(&["-d","--directory"], Store,"Name of directory holding the photos (default .)");
+	ap.refer(&mut output_dir)
+            .add_option(&["-o","--output"], Store, "Name of output directory (default /mnt/f/jpegs/)");
+	ap.refer(&mut cities)
+            .add_option(&["-c","--cities"], Store, "File holding cities names (default ./cities.csv)");
+        ap.parse_args_or_exit();
+    }
+    
+    let tab = read_cities(&cities);
+    for entry in walkdir::WalkDir::new(search_dir)
+	.max_depth(nb_levels)
         .into_iter()
         .filter_map(|e| e.ok())
     {
-        println!("{}", entry.path().display());
-        one(entry.path(), &tab, "dng");
-    }
-    /*
+        println!("Processing {}", entry.path().display());
+        one(entry.path(), &tab, "jpg",&output_dir);
+}
+
+/*
     let path = "./L1004163.DNG";
     let p = std::path::Path::new(path);
     one(p,&tab,"dng");
-    */
+*/
 }
